d78e5dfecf910c6e4e491347d0e6a44d50ecbde7
Allgamecome690 <gamernathan.ar@hotmail.com>
2026-09-02 12:51:08 -0300

    Add files via upload

---
+++ saldo.py
@@ -0,0 +1,7 @@
+saldo = float(input("Digite seu saldo: "))
+compra = float(input("Digite o valor da compra: "))
+
+if saldo >= compra:
+    print("Saldo suficiente")
+else:
+    print("Saldo Insuficiente")
---
+++ valor.py
@@ -0,0 +1,5 @@
+valor=float(input("digite o valor da compra"))
+if valor >= 200:
+    print ("Compra com Desconto")
+else:
+    print ("Compra sem Desconto")
