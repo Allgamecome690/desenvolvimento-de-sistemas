f63994e7fe038d0aba52b831fb8445bd77dd6a66
Allgamecome690 <gamernathan.ar@hotmail.com>
2026-09-16 12:58:58 -0300

    Add files via upload

--- media.py
+++ media.py
@@ -1,10 +1,13 @@
-nota1 = float(input("digite a nota 1:"))
-nota2 = float(input("digite a nota 2:"))
-nota3 = float(input("digite a nota 3:"))
-nota4 = float(input("digite a nota 4:"))
+nota1 = float(input(digite a nota 1))
+nota2 = float(input(digite a note 2))
+nota3 = float(input(digite a nota 3))
+nota4 = float(input(digite a nota 4)) 
 
-media = (nota1 + nota2 + nota3 + nota4) /4
-if media >=5:
-    print("aluno aprovado")
+media = (nota1 + nota2 + nota3 + nota4)/4
+print ("sua media é",media)
+if media <=3:
+    print("reprovado")
+elif media >=5:
+    print''''''''''''("recuperção")
 else:
     print("aluno reprovado")
